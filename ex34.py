564b4be65bce602817bcc3c2743408e720678084
AILEE\ahkim <ahkim@ahkim.com>
2016-11-06 20:49:55 +0800

    Add exercises

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(1, animals[1])
+print(2, animals[2])
+print(3, animals[0])
+print(4, animals[3])
+print(5, animals[4])
+print(6, animals[2])
+print(7, animals[5])
+print(8, animals[4])
